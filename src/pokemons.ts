14312eea9c7a0a45ab50f58760eb73b8e37f91e2
arkzuse <88483186+arkzuse@users.noreply.github.com>
2023-08-30 12:21:49 +0530

    add: pokemon object array

---
+++ src/pokemons.ts
@@ -0,0 +1,66 @@
+const imgPath: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
+
+const pokemons: {} = [
+    {
+        id: 1,
+        pokeId: 1,
+        name: 'Bulbasaur',
+        img: `${imgPath}1.png`
+    },
+    {
+        id: 2,
+        pokeId: 4,
+        name: 'Charmander',
+        img: `${imgPath}4.png`
+    },
+    {
+        id: 3,
+        pokeId: 7,
+        name: 'Squirtle',
+        img: `${imgPath}7.png`
+    },
+    {
+        id: 4,
+        pokeId: 10,
+        name: 'Caterpie',
+        img: `${imgPath}10.png`
+    },
+    {
+        id: 5,
+        pokeId: 13,
+        name: 'Weedle',
+        img: `${imgPath}13.png`
+    },
+    {
+        id: 6,
+        pokdId: 16,
+        name: 'Pidgey',
+        img: `${imgPath}16.png`
+    },
+    {
+        id: 7,
+        pokeId: 19,
+        name: 'Rattata',
+        img: `${imgPath}19.png`
+    },
+    {
+        id: 8,
+        pokeId: 21,
+        name: 'Spearow',
+        img: `${imgPath}21.png`
+    },
+    {
+        id: 9,
+        pokeId: 23,
+        name: 'Ekans',
+        img: `${imgPath}23.png`
+    },
+    {
+        id: 10,
+        pokeId: 25,
+        name: 'Pikachu',
+        img: `${imgPath}25.png`
+    }
+]
+
+export default pokemons;
